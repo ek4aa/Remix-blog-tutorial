96db45c721230ff0f8782b9c3955163b7f712b8b
ek4aa <eastking4aa@gmail.com>
2023-07-31 15:10:31 -0400

    Update: post admin and new post module

--- app/models/post.server.js
+++ app/models/post.server.js
@@ -1,5 +1,13 @@
 import { prisma } from "~/db.server";
 
+export async function createPost(post) {
+  return prisma.post.create({ data: post });
+}
+
 export async function getPosts() {
   return prisma.post.findMany();
+}
+
+export async function getPost(slug) {
+  return prisma.post.findUnique({ where: { slug } });
 }
